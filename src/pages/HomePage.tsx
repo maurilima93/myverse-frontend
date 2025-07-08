076cec20d0f888678ca65cf56bee02aca5513d08
maurilima93 <mauri.lima93@gmail.com>
2025-07-08 01:23:25 -0300

    exclude import line 5 homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Star, Users, TrendingUp, Play, ArrowRight } from 'lucide-react';
 import { Button } from '../components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../components/ui/card';
 
 const HomePage: React.FC = () => {
   const features = [
